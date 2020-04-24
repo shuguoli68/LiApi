5d799f32588e811ab6638e0cc8ca87f906334735
shu <shuguoli68@163.com>
2020-04-24 22:29:15 +0800

    user接口

--- src/main/kotlin/com/example/liapi/mapper/UserMapper.kt
+++ src/main/kotlin/com/example/liapi/mapper/UserMapper.kt
@@ -13,16 +13,19 @@ interface UserMapper {
     @Insert("INSERT INTO `user` VALUES(#{id},#{userId},#{phone},#{name},#{nickname},#{autograph},#{email},#{remarks},#{headerImg},#{vipGrade})")
     fun addUser(user: User):Int
 
-    @Delete("DELETE FROM `user` WHERE phone = #{phone}")
+    @Delete("DELETE FROM `user` WHERE user_id = #{userId}")
     fun delUser(userId: String):Int
 
     @Update("UPDATE `user`\n" +
             "        SET nick_name = #{nickname}, autograph = #{autograph}, email = #{email}, remarks = #{remarks}, header_img = #{headerImg}, vip_grade = #{vipGrade}\n" +
-            "        WHERE phone = #{phone}")
+            "        WHERE user_id = #{userId}")
     fun upUser(user: User):Int
 
     @Select("SELECT * FROM `user` WHERE user_id = #{userId}")
     fun queryById(userId:String):List<User>
+
+    @Select("SELECT * FROM `user` WHERE user_id = #{userId}")
+    fun queryByNickname(userId:String):List<User>
 
     @Select("select * from `user`")
     fun listUser():Page<User>
